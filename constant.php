610312bf9cc12695a47f51b91e9e63d2cb97c7d8
CommanderGhost <agishatake@gmail.com>
2020-04-23 15:54:21 +0700

    CONSTANT

---
+++ constant.php
@@ -0,0 +1,18 @@
+<?php
+
+// define('NAMA', 'Nama Saya');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 23;
+// echo UMUR;
+
+// class Coba
+// {
+//     const NAMA = "Nama Saya";
+// }
+
+// echo Coba::NAMA;
+
+echo __FILE__;
